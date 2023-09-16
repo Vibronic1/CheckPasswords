6184623704526c1f13584331e4baba7a0b18d1b9
Vibronic1 <70960285+Vibronic1@users.noreply.github.com>
2023-09-16 10:36:26 +0300

    Add files via upload

---
+++ CheckPasswords.py
@@ -0,0 +1,27 @@
+import re
+import sys
+
+
+if len (sys.argv) == 2:
+        f= open(sys.argv[1],'r')
+if len (sys.argv) < 3:
+    print ("Ошибка. Слишком мало параметров.")
+    sys.exit (1)
+
+if len (sys.argv) > 3:
+    print ("Ошибка. Слишком много параметров.")
+    sys.exit (1)
+
+param_name = sys.argv[1]
+param_value = sys.argv[2]
+
+if (param_name == "--File" or
+        param_name == "-f"):
+    f = open(param_value,'r')
+else:
+    print ("Ошибка. Неизвестный параметр '{}'".format (param_name) )
+    sys.exit (1)
+
+for line in f:
+    if (re.search('[!@#$%&]',line)!=None and line.count>8 and re.search('a-z',line)!=None and re.search('A-Z',line)!=None and re.search('0-9',line)!=None):
+        print (line)
